abefc421282b2362763356b71d941bff881b1068
Liana Lixandru <liana.lixandru@oracle.com>
2021-02-09 22:23:11 +0200

    Added permissions line to cloud init script

--- api/scripts/cloud_init.sh
+++ api/scripts/cloud_init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-### Send stdout and stderr to /var/log/cloud-init.log
+### Send stdout and stderr to /var/log/cloud-init2.log
 exec 1> /var/log/cloud-init2.log 2>&1
 echo "========== Get argument(s) passed thru metadata"
 VNC_PASSWORD=`curl -L http://169.254.169.254/opc/v1/instance/metadata | jq -j ".myarg_vnc_password"`
 echo "========== Change the VNC password for user opc"
 echo $VNC_PASSWORD | vncpasswd -f > /home/opc/.vnc/passwd
 chmod 600 /home/opc/.vnc/passwd
+echo "========== Change the user password for user opc"
+echo $VNC_PASSWORD | passwd --stdin opc
+echo "========== Fix SSH connection issue on some images"
+chmod 700 /home/opc
