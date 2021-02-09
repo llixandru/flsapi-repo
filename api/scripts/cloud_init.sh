d6a63f6e52dc8c62b065a61642b50640684fb719
Liana Lixandru <liana.lixandru@oracle.com>
2021-02-09 20:46:03 +0200

    changed cloud init script

--- api/scripts/cloud_init.sh
+++ api/scripts/cloud_init.sh
@@ -6,4 +6,3 @@ VNC_PASSWORD=`curl -L http://169.254.169.254/opc/v1/instance/metadata | jq -j ".
 echo "========== Change the VNC password for user opc"
 echo $VNC_PASSWORD | vncpasswd -f > /home/opc/.vnc/passwd
 chmod 600 /home/opc/.vnc/passwd
-echo "AllowUsers opc <USER>" > /etc/ssh/sshd_config
